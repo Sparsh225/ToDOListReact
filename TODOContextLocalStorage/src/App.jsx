e55a80c1fa99298c4dab58385b408db0dea473c6
Sparsh Rawat <sparshrawat34@gmail.com>
2024-01-13 19:23:58 +0530

    error removed

--- TODOContextLocalStorage/src/App.jsx
+++ TODOContextLocalStorage/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   }
   
   const updateTodo=(id,todo)=>{
-    setTodos((prev)=>(prev.map(prev)=(preTodo)=>(preTodo.id===id?todo:preTodo)))
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id?todo:prevTodo)))
   }
 
   const deleteTodo=(id)=>{
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <Todoprovider value={{todos,addTodo,deleteTodo,updateTodo,toggleComplete}}>
-      <h1 className='text-3xl font-bold underline'>Hello world</h1>
+     
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -48,7 +48,7 @@ function App() {
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
-                        {todo.map((todo)=>(
+                        {todos.map((todo)=>(
                           <div key={todo.id} className='w-full'>
                             <TodoItem todo={todo}/>
                           </div>
